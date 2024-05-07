c5a14b774c54b3000cbad46b746141d2eee3e22c
guilherme chaves <gchaves.guilherme@gmail.com>
2024-05-07 14:53:46 -0300

    chore: add rubocopa and gofmt

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,19 +1,21 @@
 return {
     "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
     config = function()
         local null_ls = require("null-ls")
         null_ls.setup({
             sources = {
                 null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prettier,
                 null_ls.builtins.formatting.prettierd,
                 null_ls.builtins.formatting.eslint,
+                null_ls.builtins.diagnostics.eslint,
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.isort,
-                null_ls.builtins.diagnostics.eslint,
+                null_ls.builtins.formatting.rubocop,
+                null_ls.builtins.diagnostics.rubocop,
+                null_ls.builtins.formatting.gofmt,
+                null_ls.builtins.formatting.goimports_reviser,
             },
         })
-
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
     end,
 }

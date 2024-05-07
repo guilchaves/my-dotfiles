0123c733aecb3f15ced1ac7bb1435a8c6a4f192b
guilherme chaves <gchaves.guilherme@gmail.com>
2024-05-07 14:53:08 -0300

    chore: updated set

--- nvim/lua/guilchaves/set.lua
+++ nvim/lua/guilchaves/set.lua
@@ -3,9 +3,8 @@ vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 4 
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
@@ -27,3 +26,4 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+

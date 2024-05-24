7f9aea158784edbec34a3853207a9e70746ce9f3
guilherme chaves <gchaves.guilherme@gmail.com>
2024-05-24 15:57:34 -0300

    feat: change lualine theme and show filename and path

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,8 +3,13 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "auto",
+				theme = "iceberg_dark",
+				icons_enabled = true,
 			},
+			sections = {
+				lualine_c = { { "filename", path = 1 } },
+			},
+			dependencies = { "nvim-tree/nvim-web-devicons" },
 		})
 	end,
 }

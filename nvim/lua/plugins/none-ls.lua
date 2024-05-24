51b50f200600e2098af248fa071ae9a22e63435b
guilherme chaves <gchaves.guilherme@gmail.com>
2024-05-24 15:59:03 -0300

    fix: remove unnescessary formatters

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -7,14 +7,10 @@ return {
             sources = {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.prettierd,
-                null_ls.builtins.formatting.eslint,
-                null_ls.builtins.diagnostics.eslint,
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.isort,
                 null_ls.builtins.formatting.rubocop,
                 null_ls.builtins.diagnostics.rubocop,
-                null_ls.builtins.formatting.gofmt,
-                null_ls.builtins.formatting.goimports_reviser,
             },
         })
     end,
